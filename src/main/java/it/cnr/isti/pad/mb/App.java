fd5a3235a0c1bb9e2905b7f0cde7992a2c351903
Marco <m.barbone@outlook.it>
2017-10-20 09:55:09 +0200

    Initial commit

---
+++ src/main/java/it/cnr/isti/pad/mb/App.java
@@ -0,0 +1,13 @@
+package it.cnr.isti.pad.mb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
